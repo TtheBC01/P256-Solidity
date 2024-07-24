8410b059955a654e80fa7217a1dbc8d4a8181232
TtheBC01 <tac688@alumni.stanford.edu>
2024-07-24 12:07:01 -0700

    update to ignition module

--- ignition/modules/Lock.ts
+++ ignition/modules/Lock.ts
@@ -1,6 +1,10 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import { parseEther } from "viem";
 
+// You can generate new valid P256 parameters at https://toddchapman.io/passkey-demo
+const QX = '0xe5cb61eef9d33263374e67681b575fd29c726f4ab58ae91fd82b6a30e0bb8db1';
+const QY = '0x3523d67086dd12a11da06fe596d361401f0083742de6b84e6044a480280e9e82';
+
 const JAN_1ST_2030 = 1893456000;
 const ONE_GWEI: bigint = parseEther("0.001");
 
@@ -8,7 +12,7 @@ const LockModule = buildModule("LockModule", (m) => {
   const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
-  const lock = m.contract("Lock", [unlockTime], {
+  const lock = m.contract("Lock", [unlockTime, QX, QY], {
     value: lockedAmount,
   });
 
